2db5a8d1bfe32550265eea0f70841a692cdf55c6
jaswanthsngp <sanagapallijaswanth@gmail.com>
2023-08-01 21:07:39 +0530

    Input for various datatypes

---
+++ Input.kt
@@ -0,0 +1,9 @@
+fun main() {
+    var age: Int;
+    var name: String;
+    println("Enter your name");
+    name= readln();
+    print("Enter your age\t");
+    age= readln().toInt();
+    println("Hi $name aged $age years!");
+}
