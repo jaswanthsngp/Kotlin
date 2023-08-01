ffa56d15a0ca712cfa8d84f178939e6e0f970725
jaswanthsngp <sanagapallijaswanth@gmail.com>
2023-08-01 11:15:37 +0530

    Data Types and Operations

--- Data Types.kt
+++ Data Types.kt
@@ -1,9 +1,12 @@
 fun main () {
     val a: Int= 234;                // Byte, Short, UByte, UShort also exist
     val b: Long= 100_100_000_000;   // ULong for unsigned long
+    // Operations: +-*/%, += -= *= /= %=, ++ --
     val c: Char= 'c';
     val d: Double= 3.14;            // Float exists too
     val e: String= "A string";
     val f: Boolean= true;
+    // Operations: && || !
+    // Bitwise Operators: shr, shl, ushr, and, or, xor, inv; Usage: a.and(b);
     println("${a}, ${b}, ${c}, ${d}, ${e}, ${f}");
 }
