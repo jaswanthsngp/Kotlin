2aa2273e3939f66e8fba92992ee1e95f660e1403
jaswanthsngp <sanagapallijaswanth@gmail.com>
2023-08-01 11:07:25 +0530

    Variables and Basic Datatypes

---
+++ Data Types.kt
@@ -0,0 +1,9 @@
+fun main () {
+    val a: Int= 234;                // Byte, Short, UByte, UShort also exist
+    val b: Long= 100_100_000_000;   // ULong for unsigned long
+    val c: Char= 'c';
+    val d: Double= 3.14;            // Float exists too
+    val e: String= "A string";
+    val f: Boolean= true;
+    println("${a}, ${b}, ${c}, ${d}, ${e}, ${f}");
+}
---
+++ Variables.kt
@@ -0,0 +1,9 @@
+val topLevelVariable= 52;
+
+fun main () {
+    val immutableVariable= 10;
+    var mutableVariable: Int;
+    mutableVariable= 4;
+    // values must be assigned with some values/a datatype at declaration
+    println("${topLevelVariable}, ${immutableVariable}, ${mutableVariable}");
+}
